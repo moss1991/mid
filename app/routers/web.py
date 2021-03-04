d6de05ec309f945e3fef82e7bc92f2801af7247b
carlos <419903711@qq.com>
2021-03-04 13:59:06 +0800

    -生成函数迁移到前端工程 20210303 rc done

--- app/routers/web.py
+++ app/routers/web.py
@@ -2,17 +2,22 @@ import requests as requests
 from fastapi import Depends,APIRouter
 from app.utils.pylog import logger
 from app.utils.pycode import formate_response,formate_response_decorator
+from app.utils.pytool import tool
 
 router = APIRouter()
 
+# @logger.catch
+# @router.get("/all")
+# async def get_xinguan_data():
+#     logger.info('call once')
+#     _request = requests.get('https://interface.sina.cn/news/wap/fymap2020_data.d.json')
+#     _data = _request.json()['data']['worldlist']
+#     # print(_data)
+#     return formate_response(200,_data,'ok')
+
 @logger.catch
-@router.get("/all")
-# @formate_response_decorator
-async def get_xinguan_data():
+@router.get("/rate")
+async def get_stock_list():
     logger.info('call once')
-    _request = requests.get('https://interface.sina.cn/news/wap/fymap2020_data.d.json')
-    _data = _request.json()['data']['worldlist']
-    # print(_data)
-    return formate_response(200,_data,'ok')
-
-
+    _rate_data = tool.get_rate()
+    return formate_response(200,_rate_data,'ok')
--- app/utils/pytool.py
+++ app/utils/pytool.py
@@ -1,5 +1,9 @@
 import os,sys
 from app.utils.pydecorators import * 
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 @singleton
 class Tool():
@@ -11,5 +15,28 @@ class Tool():
     
     def get_pwd_upper(self,file_path):
         return os.path.dirname(file_path)
+
+    def get_rate(self):
+        url = "http://www.safe.gov.cn/AppStructured/hlw/RMBQuery.do"
+        html = urlopen(url)
+        soup = BeautifulSoup(html.read(),"html.parser",from_encoding="utf-8")
+        trs = soup.find_all('tr')
+        titles = trs[9].find_all('th')
+        _rate_list = []
+
+        for i in range(10,20):
+            tr = trs[i]
+            tds = tr.find_all('td')
+            # print(tds)
+            for j in range(1,len(titles)):
+                _dict = dict({
+                    'GDATU' : ''.join(tds[0].get_text().strip().split('-')),
+                    'L_CURRECNY' : '人民币',
+                    'F_CURRENCY' : titles[j].get_text().strip(),
+                    'UKURS' : tds[j].get_text().strip() 
+                })
+                _rate_list.append(_dict)
+
+        return _rate_list
 
 tool = Tool()
